132ec920aa4956500bb31456eead8ef8bf925bd3
yaraganiveeravamsi <yaraganiveeravamsi0410@gmail.com>
2025-07-03 15:28:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
-hello i ams vamsi
+# aws provide block
+provider "aws" {
+    access_key = "AKIAWQUOZ4JWC7BC4H6X"
+    secret_key = "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"
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "vamsi" {
+  cidr_block = var.ipaddress
+  tags = {
+    name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vamsi.id
+  cidr_block = var.ipsubnet
+  tags = {
+    name = var.sub_name
+  }
+  
+}
